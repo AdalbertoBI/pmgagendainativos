5d0a15d1fb6f4a411a19f10c3c441dfb76733b08
AdalbertoBI <adalbertoparaiba2@gmail.com>
2025-07-13 15:22:39 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,37 @@
 const CACHE_NAME = 'agenda-inativos-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/map.js',
-    '/icon-192.png',
-    '/icon-512.png'
+  '/pmgagendainativos/',
+  '/pmgagendainativos/index.html',
+  '/pmgagendainativos/styles.css',
+  '/pmgagendainativos/script.js',
+  '/pmgagendainativos/map.js',
+  '/pmgagendainativos/icon-192.png',
+  '/pmgagendainativos/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        )
-    );
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(resp => resp || fetch(event.request).catch(() => {
-                console.error('Erro no fetch:', event.request.url);
-                return new Response('Offline', { status: 503 });
-            }))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request).catch(() => {
+        return new Response('Offline', { status: 503 });
+      }))
+  );
 });
