64ebfd511d29a9cfd9808b64044f9e557b328507
AdalbertoBI <adalbertoparaiba2@gmail.com>
2025-07-15 10:32:01 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agenda-inativos-v1.0.0.4'; // Troque o nome a cada deploy
+const CACHE_NAME = 'agenda-inativos-v1.0.0.5'; // Troque o nome a cada deploy
 const urlsToCache = [
   '/pmgagendainativos/',
   '/pmgagendainativos/index.html',
